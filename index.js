9689ed1a225c0070abe3413c2b5d9e5a448017ea
Claire Sersun <claire.sersun@gmail.com>
2022-05-19 09:36:59 -0400

    added array

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+var questionsArr = [
+    {
+        question: 'JavaScript is the best language',
+        answer: true
+    }, {
+        question: 'JavaScript can directly change the HTML file',
+        answer: false
+    }, {
+        question: 'Semicolons are optional but recommended if using lne breaks in JavaScript',
+        answer: true
+    }, {
+        question: 'Starting the process with pseudocode is meant to make coding in JavaScript harder',
+        answer: false
+    }, {
+        question: 'You cannot hoist expressions in JavaScript',
+        answer: true 
+    }
+]
+
+runQuiz = () => {
+    
+}
