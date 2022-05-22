d7d37b9d9d50b0dc4aae0ea181c76c7805ec6d99
Claire Sersun <claire.sersun@gmail.com>
2022-05-22 18:01:57 -0400

    10 passes

--- index.js
+++ index.js
@@ -19,14 +19,25 @@ const questionsArr = [
 
 let numCorrect = 0
   
-let runQuiz = () => {
+function runQuiz() {
+    debugger
     for (i = 0; i < questionsArr.length; i++) {
         if (confirm(questionsArr[i].question) === questionsArr[i].answer) {
             numCorrect++
         }
+    } var score = Math.round((numCorrect/questionsArr.length) * 100) +'%'
+    alert('Your quiz score is ' + score)
     }
-    let percentCorrect = numCorrect / 5
-    let unroundScore = percentCorrect * 100
-    let score = Math.round(unroundScore)
-    alert('Your quiz score is ' + score + '%')
-}
+
+
+// let runQuiz = () => {
+//     for (i = 0; i < questionsArr.length; i++) {
+//         if (confirm(questionsArr[i].question) === questionsArr[i].answer) {
+//             numCorrect++
+//         }
+//     }
+//     let percentCorrect = numCorrect / 5
+//     let unroundScore = percentCorrect * 100
+//     let score = Math.round(unroundScore)
+//     alert('Your quiz score is ' + score + '%')
+// }
